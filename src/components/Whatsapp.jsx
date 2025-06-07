5accb96389ea112c8a97bca4351d1f8492565f7f
Santosh-7124 <sunny962002@gmail.com>
2025-06-07 15:49:13 +0530

    Font change to all pages

--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Whatsapp() {
   return (
-    <a href="https://wa.me/917338658118" target="_blank" class="s-whatsapp">
+    <a href="https://wa.me/917338658118" target="_blank" className="s-whatsapp">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="48"
