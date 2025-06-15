ef62b901bdc941d4d9de5aa026544ba1c4136be6
Santosh-7124 <sunny962002@gmail.com>
2025-06-15 17:27:57 +0530

    New Logos Added

--- src/components/Home/Marquee.jsx
+++ src/components/Home/Marquee.jsx
@@ -4,6 +4,8 @@ import SEG from "../../assets/Home/Marquee/SEG.png";
 import ISK from "../../assets/Home/Marquee/ISK.png";
 import Staidrich from "../../assets/Home/Marquee/Staidrich.png";
 import Engginno from "../../assets/Home/Marquee/Engginno.png";
+import EParivahan from "../../assets/Home/Marquee/e parivahan.png";
+import Micronix from "../../assets/Home/Marquee/Micronix.png";
 
 function Marquee() {
   return (
@@ -14,26 +16,36 @@ function Marquee() {
         <img src={ISK} />
         <img src={Staidrich} />
         <img src={Engginno} />
+        <img src={EParivahan} />
+        <img src={Micronix} />
         <img src={LML} />
         <img src={SEG} />
         <img src={ISK} />
         <img src={Staidrich} />
         <img src={Engginno} />
+        <img src={EParivahan} />
+        <img src={Micronix} />
         <img src={LML} />
         <img src={SEG} />
         <img src={ISK} />
         <img src={Staidrich} />
         <img src={Engginno} />
+        <img src={EParivahan} />
+        <img src={Micronix} />
         <img src={LML} />
         <img src={SEG} />
         <img src={ISK} />
         <img src={Staidrich} />
         <img src={Engginno} />
+        <img src={EParivahan} />
+        <img src={Micronix} />
         <img src={LML} />
         <img src={SEG} />
         <img src={ISK} />
         <img src={Staidrich} />
         <img src={Engginno} />
+        <img src={EParivahan} />
+        <img src={Micronix} />
       </div>
     </div>
   );
